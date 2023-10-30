390253ebf5099ad911927b627e8a9f108a77b907
GnuWorm32 <stevanglavaskii@gmail.com>
2023-10-30 20:32:45 +0100

    Start

--- BusManagement/src/busmanagement/BusManagement.java
+++ BusManagement/src/busmanagement/BusManagement.java
@@ -10,11 +10,19 @@ package busmanagement;
  */
 public class BusManagement {
 
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainScreen ms = new MainScreen();
+        ms.setVisible(true);
+        
+        
+        
+        
     }
     
+   
 }
